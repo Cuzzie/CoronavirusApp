bb25e06d7ccbd290ec57090c3fc4e0e8e6ebf3d9
Kelwin Lee <kelwin.lee@gmail.com>
2020-04-01 23:59:10 +0800

    Made code changes based on novelcovid library changes

--- src/util/util.js
+++ src/util/util.js
@@ -1,21 +1,23 @@
-import covid from "novelcovid"
+import { NovelCovid } from "novelcovid"
 // import globalData from "../test/global.json"
 // import countriesData from "../test/countries.json"
 
+const covid = new NovelCovid()
+
 export const fetchAll = async () => {
-	const data = await covid.getAll()
+	const data = await covid.all()
 	return data
 	// return globalData
 }
 
 export const fetchCountries = async () => {
-	const data = await covid.getCountry()
+	const data = await covid.countries()
 	return data
 	// return countriesData
 }
 
 export const fetchCountry = async (country) => {
-	const data = await covid.getCountry(country)
+	const data = await covid.countries(country)
 	return data
 	// return countriesData.find(data => data.country === country)
 }
